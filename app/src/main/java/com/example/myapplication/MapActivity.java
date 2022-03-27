ca55d285b578cd3844cd175460948dbca3d72753
Thomas Liang <thmsljj@outlook.com>
2022-03-26 17:45:54 -0700

    add a getUpcoming() in User.java

--- app/src/main/java/com/example/myapplication/MapActivity.java
+++ app/src/main/java/com/example/myapplication/MapActivity.java
@@ -3,6 +3,9 @@ package com.example.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
 import androidx.appcompat.app.AppCompatActivity;
 import java.util.ArrayList;
 
@@ -14,6 +17,11 @@ public class MapActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.map_activity);
+
+
+        Button btn = new Button(this);
+
         //get user info
         //TODO
     }
